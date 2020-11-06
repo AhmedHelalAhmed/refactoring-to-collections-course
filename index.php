e27197da6e3d5e59ef3446e96c105b56118ab6cf
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2020-11-06 22:36:30 +0200

    Add illuminate/support

--- index.php
+++ index.php
@@ -1,11 +1,39 @@
 <?php
 
+require_once 'vendor/autoload.php';
 
 function load_json($path)
 {
     return json_decode(file_get_contents(__DIR__.'/'.$path), true);
 }
 
-$products = load_json('data/products.json')['products'];
+$products = collect(load_json('data/products.json')['products']);
 
-echo json_encode($products);
+// echo json_encode($products);
+$totalCost=0;
+
+// old way
+/*
+foreach ($products as $product) {
+    if ($product['product_type']=='Lamp' || $product['product_type']=='Wallet') {
+        foreach ($product['variants'] as $variant) {
+            $totalCost+=$variant['price'];
+        }
+    }
+}
+*/
+
+
+$lampsAndWallets=$products->filter(function ($product) {
+    return $product['product_type']=='Lamp' || $product['product_type']=='Wallet';
+});
+
+
+foreach ($lampsAndWallets as $product) {
+    foreach ($product['variants'] as $variant) {
+        $totalCost+=$variant['price'];
+    }
+}
+
+
+var_dump($totalCost);// 985.52
