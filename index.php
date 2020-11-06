8e81a47efc235bf53ba87a15b2659bf4f76f0561
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2020-11-06 23:12:10 +0200

    compare the two ways

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ $products = collect(load_json('data/products.json')['products']);
 
 // old way
 /*
+$totalCost=0;
 foreach ($products as $product) {
     if ($product['product_type']=='Lamp' || $product['product_type']=='Wallet') {
         foreach ($product['variants'] as $variant) {
@@ -21,6 +22,8 @@ foreach ($products as $product) {
     }
 }
 */
+
+// no loops nor levels
 
 $totalCost =$products->filter(function ($product) {
     return collect(['Lamp','Wallet'])->contains($product['product_type']);
