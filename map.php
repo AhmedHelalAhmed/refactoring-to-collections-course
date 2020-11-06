787468c786da63214bd548dc1d43a8077c98c414
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2020-11-06 23:40:29 +0200

    Add way to map strings in array

---
+++ map.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$shifts=[
+'Shipping_Steve_A7',
+'Sales_B9',
+'Support_Tara_K11',
+'J15',
+'Warehouse_B2',
+'Shipping_Dave_A6'
+];
+
+// way one
+$shiftsIds=collect($shifts)->map(function ($shift) {
+    if (strrpos($shift, '_')===false) {
+        return $shift;
+    }
+
+    $underscorePosition=strrpos($shift, '_');
+    $substringOffset=$underscorePosition+1;
+    return substr($shift, $substringOffset);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+dd($shiftsIds->all());
+
+// output ["A7","B9","K11","J15","B2","A6"]
